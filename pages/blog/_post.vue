743fddba25385afe301a8a1012ea3bd8d4fd4281
Mateo <mateo.betancur@zemoga.com>
2021-04-11 16:42:40 -0500

    first commit

---
+++ pages/blog/_post.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="post">
+    <img :src="require(`~/static/images/${post.img}`)" :alt="post.alt" />
+    <h1>{{ post.title }}</h1>
+    <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content("posts", params.post).fetch();
+    return { post };
+  },
+};
+</script>
+
+<style>
+.post {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+</style>
