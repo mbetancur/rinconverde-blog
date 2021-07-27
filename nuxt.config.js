2019794fc019b01587faa56cc79dea0d224bd3ab
Mateo Betancur <mbetanc@US-C02Z415TLVDT>
2021-07-26 23:44:57 -0500

    improving small menu styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,5 +46,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 }
