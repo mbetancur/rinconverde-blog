9b66834d1364dcb1ede4b4618200324ebf7b8d67
Mateo Betancur <mbetanc@US-C02Z415TLVDT>
2021-10-23 23:02:50 -0500

    adding carousel structure and styles-wip

---
+++ components/Carousel.vue
@@ -0,0 +1,71 @@
+<template>
+  <carousel
+    class="carousel"
+    autoplay
+    autoplayHoverPause
+    scrollPerPage
+    loop
+    mouseDrag
+    :speed="2000"
+    :perPageCustom="[
+      [100, 1],
+      [768, 3],
+    ]"
+  >
+    <slide class="carousel__slide">
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+    <slide>
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+    <slide>
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+    <slide>
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+    <slide>
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+    <slide>
+      <img
+        class="carousel__image"
+        :src="require('@/static/images/thumbnails/el-salado.jpg')"
+      />
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from "vue-carousel";
+export default {
+  components: {
+    Carousel,
+    Slide,
+  },
+};
+</script>
+
+<style lang="scss">
+.carousel {
+  background-color: ivory;
+  &__image {
+    max-width: 100%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,58 @@
 <template>
   <div class="container">
-    <video
-      :src="require('@/static/videos/video.mp4')"
-      autoplay
-      loop
-      playsinline
-      muted
-      preload="auto"
-      :poster="require('@/static/images/poster.jpg')"
-    ></video>
-    <Logo />
-    <div class="links">
-      <NuxtLink class="button--green" to="/blog"> Blog </NuxtLink>
+    <div class="container__video">
+      <video
+        :src="require('@/static/videos/video.mp4')"
+        autoplay
+        loop
+        playsinline
+        muted
+        preload="auto"
+        :poster="require('@/static/images/poster.jpg')"
+      ></video>
+      <Logo />
+      <div class="links">
+        <NuxtLink class="button--green" to="/blog"> Blog </NuxtLink>
+      </div>
     </div>
-    <carousel
-      :scrollPerPage="true"
-      :perPageCustom="[
-        [480, 2],
-        [768, 4],
-      ]"
-    >
-      <slide>
-        <img
-          src="https://picsum.photos/300/100/"
-          style="width: 300px; max-width: 100%"
-        />Slide 2 Content
-      </slide>
-      <slide>
-        <img
-          src="https://picsum.photos/300/100/"
-          style="width: 300px; max-width: 100%"
-        />Slide 1 Content
-      </slide>
-      <slide>
-        <img
-          src="https://picsum.photos/300/100/"
-          style="width: 300px; max-width: 100%"
-        />Slide 3 Content
-      </slide>
-      <slide>
-        <img
-          src="https://picsum.photos/300/100/"
-          style="width: 300px; max-width: 100%"
-        />Slide 4 Content
-      </slide>
-    </carousel>
+    <Carousel class="container__carousel" />
   </div>
 </template>
+
 <script>
-import { Carousel, Slide } from "vue-carousel";
+import Carousel from "@/components/Carousel.vue";
 export default {
   components: {
     Carousel,
-    Slide,
   },
 };
 </script>
 
 <style lang="scss">
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  video {
-    z-index: -1;
-    object-fit: cover;
-    width: 100vw;
+  .container__video {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-  }
-}
 
-.links {
-  padding-top: 15px;
+    video {
+      z-index: -1;
+      object-fit: cover;
+      width: 100vw;
+      height: 100vh;
+      position: fixed;
+      top: 0;
+      left: 0;
+    }
+    .links {
+      padding-top: 15px;
+    }
+  }
+
+  .container__carousel {
+    padding-top: 45px;
+  }
 }
 </style>
